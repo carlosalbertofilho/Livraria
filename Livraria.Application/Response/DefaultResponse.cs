96ad5204eea2f90e1d70c3e399ca566ff794d9dc
carlosalbertofilho <solrac.cassf@hotmail.com>
2024-05-07 21:53:49 -0300

    #Refactory: DefoultResponse

--- Livraria.Application/Response/DefaultResponse.cs
+++ Livraria.Application/Response/DefaultResponse.cs
@@ -1,22 +1,41 @@
-﻿namespace Livraria.Application.Response;
+﻿using Livraria.Blazor.Enum;
+
+namespace Livraria.Application.Response;
 
 public class DefaultResponse<T>
 {
-    public T? Data { get; private set; }
+    public List<T> Data { get; private set; } = [];
     public List<string> Errors { get; private set; } = [];
+    public ResponseStatus Status { get; private set; }
 
-    public DefaultResponse( T? data )
+    public DefaultResponse
+        ( List<T> data,
+        ResponseStatus status = ResponseStatus.Success )
     {
         Data = data;
+        Status = status;
+    }
+    public DefaultResponse
+        ( T data,
+        ResponseStatus status = ResponseStatus.Success )
+    {
+        Data.Add( data );
+        Status = status;
     }
 
-    public DefaultResponse(List<string> errors)
+    public DefaultResponse
+        ( List<string> errors,
+        ResponseStatus status = ResponseStatus.Error )
     {
         Errors = errors;
+        Status = status;
     }
 
-    public DefaultResponse( string errors )
+    public DefaultResponse
+        ( string errors,
+        ResponseStatus status = ResponseStatus.Error )
     {
         Errors.Add( errors );
+        Status = status;
     }
 }
